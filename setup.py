e2be8c1c9570a9809640b850e01a8433c2458eef
ackaris <37419039+ackaris@users.noreply.github.com>
2025-05-25 18:38:17 +0200

    Revert "Cleaning"
    This reverts commit 4de29e9671658ad12af41f35077e8d8be6c5bcd0.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='BRN_Load_planning',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Ali Cem Karis',
+    author_email='alicemkaris@gmail.com',
+    description=''
+)
